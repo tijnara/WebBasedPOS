a8b83ae51fa0bd2056cb2e8177485978df6582cd
tijnara <aranjitarchita@gmail.com>
2025-12-05 17:16:27 +0800

    feat: simplify Supabase client initialization and export

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,16 +1,8 @@
-// src/lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
-import * as https from "node:https";
 
-if (typeof window === 'undefined') {
-    require('dotenv').config();
-}
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
 
-// Ensure these environment variables are set in your .env.local file
-
-// Ensure these environment variables are set in your .env.local file
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-// Create a single Supabase client for interacting with your database
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export { supabase };
