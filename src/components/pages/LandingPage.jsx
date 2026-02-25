82dd1e8a0894d4368fafc01008a255e6c76e5d1c
tijnara <aranjitarchita@gmail.com>
2026-02-25 15:35:53 +0800

    style: update location text style and fix gallery description overlap

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -264,9 +264,10 @@ const SeasideWaterLanding = () => {
                         <div className="px-6 py-20 pb-32 max-w-6xl mx-auto text-center">
                             <div className="mb-16">
                              <span className="inline-block py-2 px-6 rounded-full bg-lime-100 mb-4 border border-lime-300 shadow-sm">
-    <h2 className="text-green-700 text-2xl md:text-3xl font-black tracking-widest uppercase m-0">Our Location</h2>
+    <h2 className="text-green-700 text-2xl md:text-3xl font-black tracking-widest uppercase m-0">..</h2>
 </span>
-                                <p className="text-2xl md:text-3xl font-medium text-slate-800 drop-shadow-sm">Come visit us!</p>
+                                <p>...</p>
+                                <p className="text-2xl md:text-3xl font-bold text-green-700 drop-shadow-sm">Our Location</p>
                             </div>
                             <div className="w-full relative z-30">
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3834.427589470715!2d120.1322205!3d16.043286199999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3393e1d08454d96f%3A0xfd7e1df20c90037d!2sSEASIDE%20Water%20Refilling%20Station!5e0!3m2!1sen!2sph!4v1771921863348!5m2!1sen!2sph" width="100%" height="450" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
