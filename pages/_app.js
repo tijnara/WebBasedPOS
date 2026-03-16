2ec49d255e37f26e4610545df7f4895f1d26b221
tijnara <aranjitarchita@gmail.com>
2026-03-16 09:56:01 +0800

    feat: Add contact page and link to social media

--- pages/_app.js
+++ pages/_app.js
@@ -35,7 +35,7 @@ function AuthGate({ children }) {
     useEffect(() => {
         if (sessionLoaded) {
             const isLoggedIn = !!user;
-            const isPublicPage = ['/', '/login', '/terms', '/privacy'].includes(router.pathname);
+            const isPublicPage = ['/', '/login', '/terms', '/privacy', '/contact'].includes(router.pathname);
 
             if (!isLoggedIn && !isPublicPage) {
                 console.log("AuthGate: Not logged in, redirecting to landing page");
@@ -55,7 +55,7 @@ function AuthGate({ children }) {
         );
     }
 
-    if (['/', '/login', '/terms', '/privacy'].includes(router.pathname) || user) {
+    if (['/', '/login', '/terms', '/privacy', '/contact'].includes(router.pathname) || user) {
         return children;
     }
 
@@ -75,7 +75,7 @@ export default function App({ Component, pageProps }) {
         useStore.getState().hydrate();
     }, []);
 
-    const isPublicPage = ['/', '/login', '/terms', '/privacy'].includes(router.pathname);
+    const isPublicPage = ['/', '/login', '/terms', '/privacy', '/contact'].includes(router.pathname);
     const hideNav = isPublicPage;
 
     return (
--- pages/contact.js
+++ pages/contact.js
@@ -4,7 +4,19 @@ const ContactUs = () => {
   return (
     <div>
       <h1>Contact Us</h1>
-      <p>This is the contact page.</p>
+      <p>You can reach us through our Facebook page or Messenger:</p>
+      <ul>
+        <li>
+          <a href="https://www.facebook.com/profile.php?id=61587059323111" target="_blank" rel="noopener noreferrer">
+            Facebook Page
+          </a>
+        </li>
+        <li>
+          <a href="http://m.me/94319702554752" target="_blank" rel="noopener noreferrer">
+            Messenger
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
