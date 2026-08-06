da4eb1f21e263c5c6c860b901bdc4fdb7a9066d3
tijnara <aranjitarchita@gmail.com>
2026-08-06 16:23:34 +0800

    feat: add Facebook app ID for improved Open Graph integration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -203,7 +203,7 @@ export default function App({ Component, pageProps }) {
                     <meta property="og:image" content="https://seasidepos.vercel.app/seasideHD_.png" />
                     <meta property="og:url" content="https://seasidepos.vercel.app/" />
                     <meta property="og:type" content="website" />
-                    <meta property="fb:app_id" content="" />
+                    <meta property="fb:app_id" content="27475487768800861" />
                     <link rel="icon" href="/seaside.png" />
                     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
                     <meta name="theme-color" content="#0ea5e9" />
